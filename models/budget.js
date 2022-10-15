3d04cc69cab6825fe5e408a590c7dcd9ebf22fce
Matthew <subinmatt@gmail.com>
2022-10-14 17:46:17 -0700

    Added show page with link back

--- models/budget.js
+++ models/budget.js
@@ -71,4 +71,4 @@ module.exports = [
     },
   ];
 
-  budget = module.exports;
+ 
--- server.js
+++ server.js
@@ -1,18 +1,29 @@
 const express = require('express');
+const budget = require('./models/budget.js');
 const app = express();
 const port = 3000;
 app.use(express.urlencoded({extended: false}))
 
-const budget = require('./models/budget.js');
 
 
 //              ROUTES
 
 // index route
 app.get('/budget', (req, res) => {
-  res.send('Welcome to Budgtr')
+//   res.send('Welcome to Budgtr')
+  res.render('index.ejs', {
+    allBudget: budget
+  })
 
 });
+
+// show route
+app.get('/budget/:id', (req,res) => {
+    // res.send(req.params.id)
+    res.render('show.ejs', {
+        allBudget: budget[req.params.id],
+    })
+})
 
 // listener
 app.listen(port, () => {
